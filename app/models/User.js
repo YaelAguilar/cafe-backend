6635e93876978905194f31524703ccda681074c7
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-03-14 01:26:23 -0600

    [ADD] user model

---
+++ app/models/User.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/db');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  lastname: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false // 0 => productor, 1 => comercializador
+  }
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
