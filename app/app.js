1d0d849eeb8583ad4849213674b6ca3b90060d8e
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-03-14 02:16:19 -0600

    [UPDATE] server config

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const app = require('./app/app');
+const sequelize = require('./app/database');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log('Database synced successfully.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
