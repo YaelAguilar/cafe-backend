959114cd03ff2e3d4ee451fd479e32cef9f13fb5
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-03-14 02:27:50 -0600

    [UPDATE] folder/file names & add a dependencie to ORM

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app/app');
-const sequelize = require('./app/database');
+const sequelize = require('./app/database/db');
 
 const PORT = process.env.PORT || 3000;
 
